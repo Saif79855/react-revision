e00137dc1a4274855e33e42e73933a2b0ad7ab61
Abdul Sajjad <8saifkhan@gmail.com>
2026-09-02 19:49:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Fruit, { Login, Logout, User } from "./Fruit"
+
+function App() {
+      return(
+        <>
+        <h1>First Component</h1>
+        <Fruit/>
+        <Login/>
+        <Logout/>
+        <User/>
+        </>
+      )
+}
+
+
+export default App
+
+// In react everything is component while buildinng UI 
+//First Letter of component must be Capital otherwise it will be treated as normal fun
+
+
+// for named export it is to be done in the curly braces
+
+
+ 
---
+++ src/Fruit.jsx
@@ -0,0 +1,38 @@
+function Fruit(){
+  return(
+    <div>
+      <h3>Apple</h3>
+    </div>
+  )
+}
+
+export default Fruit;
+
+
+export function Login(){
+    return(
+        <div>
+            <h1>Login</h1>
+        </div>
+    )
+}
+
+export function Logout(){
+    return(
+        <div>
+            <h1>Logout</h1>
+        </div>
+    )
+}
+
+export function User(){
+    return(
+        <div>
+            <h1>User</h1>
+        </div>
+    )
+}
+
+
+// In a file only one default export is there but can have other named exports
+// These are the example 
